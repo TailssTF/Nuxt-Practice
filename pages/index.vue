fd82acbfc340499e714b1b3b436deaf51304a5bb
TailssTF <tailss5@mail.ru>
2025-01-17 13:50:12 +0700

    feat(Main): добавил вывод списка постов

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useMainStore } from "~/store";
+
+useHead({
+  title: "Nuxt Practice",
+});
+const store = useMainStore();
+</script>
+
+<template>
+  <div>
+    <p>Список постов:</p>
+    <div v-for="post in store.posts" class="post">
+      <p>{{ post.title }}</p>
+      <p>{{ post.publishDate }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.post {
+  border: 1px solid black;
+  width: 300px;
+}
+</style>
